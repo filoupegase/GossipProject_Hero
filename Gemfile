94830125218999bc0c5688f3641f349e7aaf15e4
AlexDNZ <paleozene@gmail.com>
2019-02-04 20:25:20 +0100

    begayage de spec

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'table_print'
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 3.8'
 end
 
 group :test do
@@ -59,6 +62,10 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'shoulda-matchers', '4.0.0.rc1'
+  gem 'rails-controller-testing'
+  gem "nyan-cat-formatter"
+  gem 'rspec-rails', '~> 3.8'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  before(:each) do
+    @user = User.create(first_name: "John", last_name: "Doe")
+  end
+
+  context "validation" do
+
+    it "is valid with valid attributes" do
+      expect(@user).to be_a(User)
+      expect(@user).to be_valid
+    end
+
+    # describe "#first_name" do
+    #   it "should not be valid without first_name" do
+    #     bad_user = User.create(last_name: "Doe")
+    #     expect(bad_user).not_to be_valid
+    #     # test très sympa qui permet de vérifier que la fameuse formule user.errors retourne bien un hash qui contient une erreur concernant le first_name.
+    #     expect(bad_user.errors.include?(:first_name)).to eq(true)
+    #   end
+    # end
+    #
+    # describe "#last_name" do
+    #   it "should not be valid without last_name" do
+    #     bad_user = User.create(first_name: "John")
+    #     expect(bad_user).not_to be_valid
+    #     expect(bad_user.errors.include?(:last_name)).to eq(true)
+    #   end
+    # end
+
+   end
+
+
+end
